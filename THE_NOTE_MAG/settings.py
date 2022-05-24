51d64ca6b0406b3b64aa94351d269c21859110cc
KevinDGnanih <kevin.gnanih@gmail.com>
2022-05-24 20:43:34 +0000

    code

--- THE_NOTE_MAG/settings.py
+++ THE_NOTE_MAG/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
