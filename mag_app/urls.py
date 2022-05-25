d99f46d0f4b02a46e39934eadd1602607c638308
KevinDGnanih <kevin.gnanih@gmail.com>
2022-05-25 20:31:41 +0000

    Created the urls for the CRUD system

--- mag_app/urls.py
+++ mag_app/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
+    path('add_post/', views.AddPost.as_view(), name='add_post'),
+    path('edit_post/<slug:slug>', views.EditPost.as_view(), name='edit_post'),
+    path('<slug:slug>/delete', views.DeletePost.as_view(), name='delete_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
