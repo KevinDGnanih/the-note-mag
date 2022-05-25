b9ba37020ca801e8410a1f28355cacde2aebd063
KevinDGnanih <kevin.gnanih@gmail.com>
2022-05-25 19:33:28 +0000

    Added filter by category in admin page

--- mag_app/admin.py
+++ mag_app/admin.py
@@ -8,7 +8,7 @@ from .models import Post, Comment, Category
 class PostAdmin(SummernoteModelAdmin):
     """ Structure the admin Post field with summernote and django """
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('status', 'created_on')
+    list_filter = ('status', 'category', 'created_on')
     list_display = ('title', 'slug', 'category', 'status', 'created_on')
     search_fields = ['title', 'content']
     summernote_fields = ('content')
